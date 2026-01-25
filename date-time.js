f78405e4162f34432a7e68626189e6e37e08aef5
Hello World !!! <anisharawat324@gmail.com>
2026-01-25 20:26:11 +0530

    Update date-time.js

--- date-time.js
+++ date-time.js
@@ -24,3 +24,27 @@ console.log(myDate.getMinutes())             // print current minutes
 console.log(myDate.getSeconds())             // print current seconds  
 console.log(myDate.getMonth()+1)             // print current motnh of the year(start from 1) -> (1)
 console.log(myDate.getFullYear())            // print current year -> (2026)
+
+//--------------------------------------------------------------------------------------------------------------------
+console.log("\n")
+
+let newDate=new Date(2026,7,14,22,30,3)      // this will create new date-time from the input data
+console.log(newDate.toLocaleString())
+
+let x=new Date(2026,0,4)
+console.log(x.toLocaleString())
+
+let y=new Date('2026-07-24')
+console.log(y.toLocaleString())
+
+let z=new Date('08-14-2026')
+console.log(z.toLocaleString())
+
+console.log(`${z.getDate()} is the date , ${z.getMonth()+1} is the month , ${z.getFullYear()} is the year in date ${z.toLocaleString()}`)
+
+//--------------------------------------------------------------------------------------------------------------------
+console.log('\n')
+
+let nowDate=Date.now()
+console.log(nowDate)
+console.log(Math.floor(nowDate/1000))
